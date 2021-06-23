f303733ef38d0cb7d1b65ee0b94447875b005feb
careymwarabu <careyakmwarabu@gmail.com>
2021-06-23 21:40:08 +0300

    add config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+
+
+class Config:
+    """
+    General configuration class
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carey:carey@localhost/blog'
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+class ProdConfig(Config):
+    """
+    Production configuration class
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+    
+class DevConfig(Config):
+    """
+    Development configuration class
+    """
+    DEBUG = True
+    
+    
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://carey:carey@localhost/test_blog'
+
+
+config_option = {
+    'production': ProdConfig,
+    'development': DevConfig,
+    'test': TestConfig
+}
